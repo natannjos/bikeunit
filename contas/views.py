911309cc1896b037778fd38387a605f07b44f0d0
Natan Oliveira <natannjos@gmail.com>
2018-11-30 20:57:35 -0200

    test_meus_grupos: sessoes pre criadas, move verificação de login para base

--- contas/views.py
+++ contas/views.py
@@ -7,12 +7,12 @@ from django.core.urlresolvers import reverse
 def envia_email_login(request):
     email = request.POST['email']
     token = Token.objects.create(email=email)
-    
+
     url = request.build_absolute_uri(
         reverse('login') + '?token=' + str(token.uid)
     )
     corpo_do_email = f'Use este link para logar:\n\n{url}'
-    
+
     send_mail('Seu link para login no BikeUnit',
               corpo_do_email,
               'noreply@bikeunit.com', 
